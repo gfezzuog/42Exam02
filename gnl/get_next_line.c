ede0ab0882396eee1a10ef4da7b900a586f7f6e2
gfezzuog <nanaki1307@gmail.com>
2023-04-05 00:43:18 +0200

    right gnl check the .h

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -2,53 +2,23 @@
 
 char *get_next_line(int fd)
 {
-    int i = -1;
-    char *buffer;
-    char c;
-    buffer = malloc(50000);
-    while((read(fd, &c, BUFFER_SIZE * 0 + 1) > 0))
-    {
-        buffer[++i] = c;
-        if(buffer[i] == '\n')
-            break;
-    }
-    if (!buffer[i])
-    {
-        free(buffer);
-        return(NULL);
-    }
-    i++;
-    buffer[i] = '\0';
-    return(buffer);
-}
+		int i = - 1;
+		int rd;
+		char *buffer = malloc(999999);
+		char c;
 
-void ft_putstr(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		write(1, &str[i], 1);
+		while((rd = read(fd, &c, BUFFER_SIZE * 0 + 1)) > 0)
+		{
+				buffer[++i] = c;
+				if (buffer[i] == '\n')
+						break;
+		}
+		if ((!buffer[i] && !rd) || rd == - 1 || BUFFER_SIZE == 0)
+		{
+				free(buffer);
+				return(NULL);
+		}
 		i++;
-	}
-	return ;
-}
-
-int main(int argc, char **argv)
-{
-	int fd;
-	char *line;
-
-	(void)argc;
-	fd = open(argv[1], O_RDONLY);
-	line = NULL;
-	while ((line = get_next_line(fd)))
-	{
-		ft_putstr(line);
-		free(line);
-	}
-	free(line);
-	close(fd);
-	return (0);
+		buffer[i] = '\0';
+		return(buffer);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -1,11 +1,9 @@
 #ifndef GET_NEXT_LINE_H
 #   define GET_NEXT_LINE_H
-
-#   include<unistd.h>
-#   include<stdlib.h>
-#   include<fcntl.h>
-
-#   define BUFFER_SIZE 1
-
-char    *get_next_line(int fd);
+#   ifndef  BUFFER_SIZE
+#        define BUFFER_SIZE 1
+#   endif
+#   include <unistd.h>
+#   include <stdlib.h>
+char *get_next_line(int fd);
 #endif
